861db12d377012b43f62d652368c8c51ee752abc
muhacoder011 <muhacoder2011@gmail.com>
2026-07-15 22:19:56 +0500

    kommentariya

--- find_bug.py
+++
@@ -1,9 +0,0 @@
-with open("index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-import re
-matches = [m.start() for m in re.finditer(r"fa-user", html)]
-print(f"Found {len(matches)} occurrences of fa-user:")
-for m in matches:
-    print(html[max(0, m-200):m+200])
-    print("---")
